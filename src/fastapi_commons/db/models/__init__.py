0562368727717547b0467a16d43f4ef054adcf82
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-10-28 23:39:06 +0200

    removed some auth models in favor of python3_commons

--- src/fastapi_commons/db/models/__init__.py
+++ src/fastapi_commons/db/models/__init__.py
@@ -1,6 +1,4 @@
 from fastapi_commons.db.models.auth import ApiKey as ApiKey
-from fastapi_commons.db.models.auth import User as User
-from fastapi_commons.db.models.auth import UserGroup as UserGroup
 from fastapi_commons.db.models.rbac import RBACApiKeyRole as RBACApiKeyRole
 from fastapi_commons.db.models.rbac import RBACPermission as RBACPermission
 from fastapi_commons.db.models.rbac import RBACRole as RBACRole
--- src/fastapi_commons/db/models/auth.py
+++ src/fastapi_commons/db/models/auth.py
@@ -1,24 +1,11 @@
 import uuid
 from datetime import datetime
 
-from fastapi_users_db_sqlalchemy import GUID, SQLAlchemyBaseUserTableUUID
+from fastapi_users_db_sqlalchemy import GUID
 from python3_commons.db import Base
-from python3_commons.db.models.common import BaseDBModel, BaseDBUUIDModel
-from sqlalchemy import BIGINT, DateTime, ForeignKey, String
+from python3_commons.db.models.common import BaseDBUUIDModel
+from sqlalchemy import DateTime, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
-
-
-class UserGroup(BaseDBModel, Base):
-    __tablename__ = 'user_groups'
-
-    name: Mapped[str] = mapped_column(String, nullable=False)
-
-
-class User(SQLAlchemyBaseUserTableUUID, Base):
-    __tablename__ = 'users'
-
-    username: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-    group_id: Mapped[int | None] = mapped_column(BIGINT, ForeignKey('user_groups.id'))
 
 
 class ApiKey(BaseDBUUIDModel, Base):
