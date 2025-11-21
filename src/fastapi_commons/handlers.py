15cc41ff7d101e0d9ba9f5e81f764d74a190dd73
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-11-21 18:55:36 +0200

    Improved exception handlers

--- src/fastapi_commons/handlers.py
+++ src/fastapi_commons/handlers.py
@@ -31,19 +31,34 @@ def handle_exceptions(func):
 
             return _handle_exceptions_helper(HTTPStatus.INTERNAL_SERVER_ERROR, *e.args)
         except PermissionError as e:
-            logger.exception('Permission error.')
+            msg = f'{type(e)}: {e}'
+            logger.exception(msg)
+
             return _handle_exceptions_helper(HTTPStatus.UNAUTHORIZED, *e.args)
         except LookupError as e:
-            logger.exception('Lookup error.')
+            msg = f'{type(e)}: {e}'
+            logger.exception(msg)
+
             return _handle_exceptions_helper(HTTPStatus.NOT_FOUND, *e.args)
         except ValidationError as e:
-            logger.exception('Validation error.')
+            msg = f'{type(e)}: {e}'
+            logger.exception(msg)
+
             return _handle_exceptions_helper(HTTPStatus.INTERNAL_SERVER_ERROR, *e.args)
+        except ConnectionRefusedError as e:
+            msg = f'{type(e)}: {e}'
+            logger.exception(msg)
+
+            return _handle_exceptions_helper(HTTPStatus.SERVICE_UNAVAILABLE, *e.args)
         except ValueError as e:
-            logger.exception('Value error.')
+            msg = f'{type(e)}: {e}'
+            logger.exception(msg)
+
             return _handle_exceptions_helper(HTTPStatus.BAD_REQUEST, *e.args)
-        except NotImplementedError:
-            logger.exception('Not implemented.')
+        except NotImplementedError as e:
+            msg = f'{type(e)}: {e}'
+            logger.exception(msg)
+
             return _handle_exceptions_helper(HTTPStatus.NOT_IMPLEMENTED)
 
     return wrapper
